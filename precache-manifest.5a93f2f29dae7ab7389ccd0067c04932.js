74e4fdac078f765813c53c09dfedc810b4b48e25
my-dev-git <siva.theme@gmail.com>
2020-05-04 21:41:17 +0530

    First commit

---
+++ precache-manifest.5a93f2f29dae7ab7389ccd0067c04932.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36eb30efd30b2f1311d3fff32546897",
+    "url": "/matrimonial_v2/index.html"
+  },
+  {
+    "revision": "bfdf3204d8d317c97545",
+    "url": "/matrimonial_v2/static/js/2.f64e2b38.chunk.js"
+  },
+  {
+    "revision": "a31c8917a9e923f51407",
+    "url": "/matrimonial_v2/static/js/main.bd5bf232.chunk.js"
+  },
+  {
+    "revision": "d07a29c40b1c54a4fcdf",
+    "url": "/matrimonial_v2/static/js/runtime~main.9dc68cc6.js"
+  }
+]);
